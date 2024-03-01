923d893fb52db0c126d7b545f9f3c7e33539e2b1
assil01 <assilbm0@gmail.com>
2024-03-01 16:54:38 +0100

    fix register and JWT token

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
+connectDB();
 
 app.use('/api/users/',userRoutes);
 app.get('/',(req, res)=>res.send('server is ready'));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, ()=> console.log("Server start on port ",{port}));
